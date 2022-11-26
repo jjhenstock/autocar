f203d4237506eb7b16c7b18ff194f08aa8ec3241
jjhenstock <63672596+jjhenstock@users.noreply.github.com>
2022-11-26 12:14:19 -0600

    version: 0.1.2
    updated merge pdf

--- autocar/merge_pdf.py
+++ autocar/merge_pdf.py
@@ -1,47 +1,57 @@
-try:
-    from PyPDF2 import PdfFileMerger
-    import os
-    import warnings
-    import re
-except ModuleNotFoundError:
-    print('Ensure all dependencies are installed.')
-    _ = input('Press Enter to exit')
-    exit()
+from PyPDF2 import PdfFileMerger
+import warnings
+import re
+import xlwings as xw
+from pathlib import Path
 
 # Ignore warnings
 warnings.filterwarnings('ignore')
 
-# Input
-user_name = input('Please enter your initials\n')
+# Set workbook variables
+xl_app = xw.apps.active
+wb: xw.Book = xw.Book.caller()
+ws_input: xw.Sheet = wb.sheets['Input']
 
-index_names = ['batch', 'index', 'cover', 'start']
-pdfs = []
-current_dir = os.path.dirname(os.path.realpath(__file__))
-directory = os.fsencode(current_dir)
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    if filename.endswith('.pdf') or filename.endswith('.PDF'):
-        if any(_ in filename.lower() for _ in index_names):
-            index = filename
-        else:
-            pdfs.append(filename)
 
-pdfs.sort()
-try:
-    pdfs.insert(0, index)
-    list_index = re.findall(r'\d+', index)
-    str_index = ''.join(list_index)
-    result = user_name + ' ' + str_index + '.pdf'
-except NameError:
-    result = 'Merged.pdf'
-merger = PdfFileMerger()
-try:
+def main(current_dir):
+    current_dir = str(current_dir).replace('/', '\\')
+
+    # Input
+    user_name = ws_input['c_user'].value
+
+    index_names = ['batch', 'index', 'cover', 'start']
+    pdfs = []
+
+    directory = Path(current_dir)
+
+    for file in directory.iterdir():
+        filename = file.name
+        if filename.endswith('.pdf') or filename.endswith('.PDF'):
+            if any(_ in filename.lower() for _ in index_names):
+                index = file
+            else:
+                pdfs.append(file)
+
+    pdfs.sort()
+    try:
+        pdfs.insert(0, index)
+        list_index = re.findall(r'\d+', index.stem)
+        str_index = ''.join(list_index)
+        result = user_name + ' ' + str_index + '.pdf'
+    except NameError:
+        result = 'Merged.pdf'
+
+    merger = PdfFileMerger()
     for pdf in pdfs:
         merger.append(pdf)
-
-    merger.write(result)
+    merger.write((directory / result))
     merger.close()
-except Exception:
-    _ = input('Error: There is a problem with a file you are trying to Merge')
+    xl_app.alert('Complete', title='Complete', mode='info')
 
-_ = input('Complete! Press Enter to exit')
+
+if __name__ == "__main__":
+    xw.Book('Merge PDF.xlsb').set_mock_caller()
+
+    # wb: xw.Book = xw.Book.caller()
+    # ws_input: xw.Sheet = wb.sheets['Input']
+    main()
